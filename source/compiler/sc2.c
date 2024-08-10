f3dd2c17117634ec5a1f1a166ee5c742ec53f3a1
James L. Walsh <jloubierwalsh@gmail.com>
2024-08-10 16:54:32 -0700

    Fix preprocessor hanging indefinitely when running #include on WSL

--- source/compiler/sc2.c
+++ source/compiler/sc2.c
@@ -21,6 +21,9 @@
  *  Version: $Id: sc2.c 3655 2006-10-23 20:17:52Z thiadmer $
  */
 
+// Ensures stat() does not overflow when running on 64 bit systems
+#define _FILE_OFFSET_BITS 64
+
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -164,8 +167,8 @@ static char extensions[][6] = { "", ".inc", ".p", ".pawn" };
             *ptr=DIRSEP_CHAR;
       }
     #endif
-    stat(real_path, &st);
-    if (!S_ISDIR(st.st_mode))   /* ignore directories with the same name */
+    int sr = stat(real_path, &st);
+    if (sr == 0 && !S_ISDIR(st.st_mode))   /* ignore directories with the same name */
       fp=(FILE*)pc_opensrc(real_path);
     if (fp==NULL) {
       *ext='\0';                /* on failure, restore filename */
